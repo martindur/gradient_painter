fc20da0ed20d35fe4455308b1b7b3d393e5654a7
Martin Durhuus <martindurhuus@gmail.com>
2017-05-02 16:32:22 +0200

    Removed export(for now). Focus on painting interaction.

--- gradient_painter.py
+++ gradient_painter.py
@@ -1,6 +1,3 @@
-import bpy
-from bpy.props import *
-
 bl_info = {
     "name" : "Gradient Painter",
     "author" : "Martin Durhuus",
@@ -12,18 +9,54 @@ bl_info = {
     "category" : "Paint",
 }
 
+import bpy
+from bpy.props import (
+        StringProperty,
+        BoolProperty,
+        FloatProperty,
+        IntProperty,
+        EnumProperty,
+        CollectionProperty,
+        )
+
 image_to_export = None
 node_val_ramp = None
 node_col_ramp = None
+
+object_attr = {}
+#Keep track of the main shader
+mat = None
 
 def init_settings(context):
     context.space_data.viewport_shade = 'MATERIAL'
     context.scene.render.engine = 'CYCLES'
 
-def create_new_image(context, name):
-    image = bpy.data.images.new(name=(name + "_col"), width=1, height=256)
+def create_new_image(context, name, tex_type='COL'):
+    tex_width = context.scene.texture_width
+    tex_height = context.scene.texture_height
+    if tex_type == 'AO':
+        image = bpy.data.images.new(name=(name + "_ao"), width=tex_width, height=tex_height)
+    elif tex_type == 'COL':
+        image = bpy.data.images.new(name=(name + "_col"), width=tex_width, height=tex_height)
     return(image)
 
+def texture_baking(context, mat, image, bake_type='DIFFUSE'):
+    nodes = mat.node_tree.nodes
+    scn = context.scene
+    global object_attr
+    image_node = nodes.new("ShaderNodeTexImage")
+    image_node.image = image
+    nodes.active = image_node
+    scn.cycles.bake_type = bake_type
+    if bake_type == 'AO':
+        object_attr['AO_node'] = image_node
+    elif bake_type == 'DIFFUSE':
+        object_attr['DIF_node'] = image_node
+        bake_settings = bpy.data.scenes[scn.name].render.bake
+        bake_settings.use_pass_color = True
+        bake_settings.use_pass_direct = False
+        bake_settings.use_pass_indirect = False
+    bpy.ops.object.bake(bake_type)
 
 def create_base_material(context, mesh_name):
     mat = bpy.data.materials.new(mesh_name + "_mat")
@@ -47,6 +80,12 @@ def create_gradient_material(context, mesh_name):
     node_diffuse = nodes.new("ShaderNodeBsdfDiffuse")
     node_diffuse.location.x = tmp_loc.x - margin
     tmp_loc = node_diffuse.location
+    #Mix node set to Multiply(Specifically for AO)
+    node_multiply_ao = nodes.new("ShaderNodeMixRGB")
+    node_multiply_ao.blend_type = 'MULTIPLY'
+    node_multiply_ao.inputs['Fac'].default_value = 1.0
+    node_multiply_ao.location.x = tmp_loc.x - margin
+    tmp_loc = node_multiply_ao.location
     #Mix node set to Multiply
     node_multiply = nodes.new("ShaderNodeMixRGB")
     node_multiply.blend_type = 'MULTIPLY'
@@ -80,6 +119,15 @@ def create_gradient_material(context, mesh_name):
     node_UV_map.uv_map = "ProjectionMap"
     node_UV_map.location.x = tmp_loc.x - margin
     
+    #Assign to object's attributes
+    global object_attr
+    object_attr['Material'] = mat
+    object_attr['Col_ramp'] = node_col_ramp
+    object_attr['Val_ramp'] = node_val_ramp
+    object_attr['BSDF_node'] = node_diffuse
+    object_attr['Mix_node'] = node_multiply_ao
+    object_attr['Output_node'] = node_output
+
     #
     #Node linking(Left to right)
     #
@@ -90,69 +138,42 @@ def create_gradient_material(context, mesh_name):
     node_links.new(node_gradient_tex.outputs[0], node_val_ramp.inputs[0])
     node_links.new(node_col_ramp.outputs[0], node_multiply.inputs[1])
     node_links.new(node_val_ramp.outputs[0], node_multiply.inputs[2])
-    node_links.new(node_multiply.outputs[0], node_diffuse.inputs[0])
+    node_links.new(node_multiply.outputs[0], node_multiply_ao.inputs[0])
+    node_links.new(node_multiply_ao.outputs[0], node_diffuse.inputs[0])
     node_links.new(node_diffuse.outputs[0], node_output.inputs[0])
-    
-    #
-    #Create texture image for baking
-    #
-    image = create_new_image(context, mesh_name)
-    global image_to_export
-    image_to_export = image
-    image_node = nodes.new(type="ShaderNodeTexImage")
-    image_node.location = node_output.location
-    image_node.location.y -= margin
-    image_node.image = image
-    
-    #Set active node
-    nodes.active = image_node
-    return(mat)
 
-def create_composite(context, image, path):
-    context.scene.use_nodes = True
-    composite_nodes = context.scene.node_tree.nodes
-    composite_nodes.clear()
-    #Improper way to enable backdrop, but oh well..
-    area = context.area
-    orig_area = area.type
-    area.type = 'NODE_EDITOR'
-    space_data = area.spaces.active
-    space_data.show_backdrop = True
-    area.type = orig_area
-    ##
-    links = context.scene.node_tree.links
-    #Node creation
-    cnode_output = composite_nodes.new("CompositorNodeOutputFile")
-    cnode_image = composite_nodes.new("CompositorNodeImage")
-    cnode_blur = composite_nodes.new("CompositorNodeBlur")
-    #Node settings
-    cnode_blur.filter_type = 'FAST_GAUSS'
-    cnode_blur.use_relative = True
-    cnode_blur.factor_y = 5
-    cnode_image.image = image
-    cnode_output.base_path = path
-    #Node linking
-    links.new(cnode_image.outputs[0], cnode_blur.inputs[0])
-    links.new(cnode_blur.outputs[0], cnode_output.inputs[0])
+    return(mat)
     
 def handle_projection(context):
-    if context.object.data.uv_textures.active_index == 0 or\
-    len(context.object.data.uv_textures) == 0:
-        bpy.ops.uv.smart_project(angle_limit=66,
-                                island_margin=0.02,
-                                user_area_weight=0,
-                                use_aspect=False,
-                                stretch_to_bounds=True
-                                )
-    for uv in context.object.data.uv_textures:
+    gen_uv = context.scene.generate_UV
+    uv_textures = context.object.data.uv_textures
+    if gen_uv:
+        if len(uv_textures) == 0:    
+            bpy.ops.uv.smart_project(angle_limit=66,
+                                    island_margin=0.02,
+                                    user_area_weight=0,
+                                    use_aspect=False,
+                                    stretch_to_bounds=True
+                                    )
+        elif uv_textures.active.name == "ProjectionMap" and \
+        len(uv_textures) == 1:
+            uv_map = bpy.ops.mesh.uv_texture_add()
+            bpy.ops.uv.smart_project(angle_limit=66,
+                                    island_margin=0.02,
+                                    user_area_weight=0,
+                                    use_aspect=False,
+                                    stretch_to_bounds=True
+                                    )
+    for index, uv in enumerate(uv_textures):
         if uv.name == "ProjectionMap":
             projection_map = uv
+            uv_textures.active_index = index
             bpy.ops.uv.project_from_view(orthographic=True,
                                          scale_to_bounds = True
                                          )
             return projection_map
     bpy.ops.mesh.uv_texture_add()
-    projection_map = context.object.data.uv_textures[-1]
+    projection_map = uv_textures[-1]
     projection_map.name = "ProjectionMap"
     bpy.ops.uv.project_from_view(orthographic=True,
                                  scale_to_bounds = True
@@ -161,7 +182,7 @@ def handle_projection(context):
 
 class MenuPanel(bpy.types.Panel):
     bl_idname = "paint.gradient_texturing"
-    bl_label = "Gradient Texturing"
+    bl_label = "Gradient Painter"
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     bl_category = "Arura"
@@ -169,39 +190,42 @@ class MenuPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         scn = context.scene
-        
+        ob = context.active_object
+
         col = layout.column(align=False)
-        col.label(text="Gradient from view:")
+        col.label(text="Gradient from View:")
         row = col.row(align=False)
         row.operator("bake.process_mesh")
-        row = layout.row()
-        col.label(text="Export Destination:")
-        layout.prop(scn, 'file_dir')
-        row = layout.row()
-        row.operator("bake.export_tex_mesh")
         
-        layout.label("Texture Control:")
         row = layout.row()
-        ob = context.active_object
+        layout.prop(scn, "generate_UV")
+
+        layout.label("Baking:")
+        row = layout.row()
+        layout.prop(scn, "enable_ao")
+        layout.prop(scn, "texture_width")
+        layout.prop(scn, "texture_height")
+        row.operator("bake.bake_texture")
+
         if len(ob.material_slots.items()) > 0:
             ob_mat = ob.material_slots[0]
             try:
+                layout.label("Texture Control:")
+                row = layout.row()
                 node_col_ramp = ob_mat.material.node_tree.nodes['Color']
                 layout.template_color_ramp(node_col_ramp, "color_ramp", expand=True)
-            except:
-                pass
-            try:
                 node_val_ramp = ob_mat.material.node_tree.nodes['Value']
                 layout.template_color_ramp(node_val_ramp, "color_ramp", expand=True)
             except:
                 pass
         
 class ProcessMesh(bpy.types.Operator):
-    """Unwraps, applies material and adds empty texture"""
+    """Unwraps if required and applies gradient shader"""
     bl_idname = "bake.process_mesh"
-    bl_label = "Update"
+    bl_label = "Calculate"
     bl_options = {'REGISTER', 'UNDO'}
     
+
     def execute(self, context):
         if context.active_object:
             init_settings(context)
@@ -219,56 +243,68 @@ class ProcessMesh(bpy.types.Operator):
             self.report({'INFO'}, "Select an object to process")
             return {'FINISHED'}
 
-class Export(bpy.types.Operator):
-    """Exports both the mesh and texture into selected folder"""
-    bl_idname = "bake.export_tex_mesh"
-    bl_label = "Export"
+class BakeTexture(bpy.types.Operator):
+    """Creates texture and bakes an appropriate map"""
+    bl_idname = "bake.bake_texture"
+    bl_label = "Bake"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     def execute(self, context):
+        enable_ao = context.scene.enable_ao
+
         if context.active_object:
             ob = context.active_object
-            scn = context.scene
-            global image_to_export
-            #
-            #Setup bake settings and do the bake
-            #
-            scn.cycles.bake_type = 'DIFFUSE'
-            bake_settings = bpy.data.scenes[scn.name].render.bake
-            bake_settings.use_pass_color = True
-            bake_settings.use_pass_direct = False
-            bake_settings.use_pass_indirect = False
-            bpy.ops.object.bake(type='DIFFUSE')
-            #First export comes out blank. Must have something to do with the order.
-            #composite_image = bpy.data.images['Viewer Node']
-            export_path = bpy.data.scenes[scn.name].file_dir
-            ###Should use filename_path so it doesn't save out to 'image0001', but can't fix it as of yet.
-            #filename_path = (export_path + image_to_export.name + ".png")
-            #create_composite(context, image_to_export, export_path)
-            self.fbx_export(context, export_path, ob.name)
-            #This is currently a simpler way to save the image to file.
-            #bpy.ops.render.render()
-            image_to_export.save_render(export_path + image_to_export.name + ".png")
+            global mat
+            if mat is None:
+                self.report({'WARNING'}, "Calculate the gradient before baking!")
+                return {'CANCELLED'}
+            image_dif = create_new_image(context, ob.name)
+            texture_baking(context, mat, image_dif)
+            if enable_ao:
+                image_ao = create_new_image(context, ob.name, tex_type='AO')
+                texture_baking(context, mat, image_ao, bake_type='AO')
             return {'FINISHED'}
-    
-    def fbx_export(self, context, file_dir, name):
-        #Reserved for possible presets.
-        bpy.ops.export_scene.fbx(filepath = (file_dir + name + ".fbx"), use_selection=True, object_types={'MESH'})
-        return {'FINISHED'}
+        else:
+            self.report({'INFO'}, "Select a valid object for baking")
 
 def register():
-    bpy.types.Scene.file_dir = None
-    bpy.types.Scene.file_dir = StringProperty(name="Path", subtype="FILE_PATH")
-    
+    #Scene properties
+    bpy.types.Scene.generate_UV = BoolProperty(
+        name="Generate UV",
+        description="Generate new UV map for mesh "
+        "(WARNING: Will remove existing UV map)",
+        default=False,
+    )
+
+    bpy.types.Scene.texture_width = IntProperty(
+        name="Width: ",
+        description="Width of the texture bake",
+        default=512,
+    )
+
+    bpy.types.Scene.texture_height = IntProperty(
+        name="Height: ",
+        description="Height of the texture bake",
+        default=512,
+    )
+
+    bpy.types.Scene.enable_ao = BoolProperty(
+        name="Enable AO: ",
+        description="Enables Ambient Occlusion for baking",
+        default=False,
+    )
     bpy.utils.register_class(ProcessMesh)
-    bpy.utils.register_class(Export)
     bpy.utils.register_class(MenuPanel)
+    bpy.utils.register_class(BakeTexture)
     
 def unregister():
-    del bpy.types.Scene.file_dir
+    del bpy.types.Scene.generate_UV
+    del bpy.types.Scene.texture_width
+    del bpy.types.Scene.texture_height
+    del bpy.types.Scene.enable_ao
     bpy.utils.unregister_class(ProcessMesh)
-    bpy.utils.unregister_class(Export)
     bpy.utils.unregister_class(MenuPanel)
+    bpy.utils.unregister_class(BakeTexture)
     
 if __name__ == '__main__':
     register()
